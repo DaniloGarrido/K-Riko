a8490a86229e07c6fb95bda1b6933a48a2381a32
DaniloGarrido <danilo.garrido.developer@gmail.com>
2026-01-22 01:41:43 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // Usar base relativa './' es lo más seguro para GitHub Pages 
-  // ya que funciona independientemente del nombre del repositorio.
-  base: './',
+  // Forzamos el nombre del repositorio para asegurar que los assets carguen
+  base: '/K-Riko/',
 })
